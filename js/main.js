8dff9865d57642d4f862a57671c1d6c52e1f0082
Vladimir Jimenez <vladimir.jimenez@smgov.net>
2015-10-13 16:22:53 -0700

    Added basic filtering; Just need to add margin

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  var $sourceContainers = $(".source-container");
+  
+  $("#search").keyup(function () {
+    var $this = $(this);
+    var searchParam = $this.val().toLowerCase();
+    
+    if (searchParam.length >= 3) {
+      $sourceContainers.addClass("hidden");
+      $sourceContainers.each(function () {
+        var $this = $(this);
+        var header = $this.find("h2").html().toLowerCase();
+        var body = $this.find("p").html().toLowerCase();
+        
+        if (header.indexOf(searchParam) >= 0 || body.indexOf(searchParam) >= 0) {
+          $this.removeClass("hidden");
+        }
+      });
+    } else {
+      $sourceContainers.removeClass("hidden");
+    }
+  });
+});
